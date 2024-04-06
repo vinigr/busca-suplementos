1a08dddd5bbc69b2dcc7af3314312b3423377874
Vinícios Grama <vinyirun1@gmail.com>
2024-04-06 13:57:33 -0300

    feat: get products by company

--- src/routes/client/companies.ts
+++ src/routes/client/companies.ts
@@ -3,9 +3,59 @@ import { db } from "../../db/db";
 
 export const companiesClientRoutes = new Elysia({
   prefix: "/companies",
-}).get("images", async () => {
-  return db.companies
-    .where({ active: true })
-    .select("slug", "name", "urlImage")
-    .order("slug");
-});
+})
+  .get("images", async () => {
+    return db.companies
+      .where({ active: true })
+      .select("slug", "name", "urlImage")
+      .order("slug");
+  })
+  .get(
+    ":slug/products",
+    async ({ params: { slug }, set }) => {
+      const company = await db.companies
+        .findByOptional({ slug, active: true })
+        .select("id");
+
+      if (!company) {
+        set.status = 404;
+
+        return { error: "Marca não encontrada" };
+      }
+
+      const productsTypesCompany = await db.products
+        .where({ companyId: company.id })
+        .select("productTypeId");
+
+      const productsTypesIds = productsTypesCompany.map(
+        (product) => product.productTypeId
+      );
+
+      return await db.productsTypes
+        .whereIn({ id: productsTypesIds })
+        .select("slug", "name", {
+          products: (q) =>
+            q.products
+              .where({ companyId: company.id })
+              .select(
+                "slug",
+                "urlImage",
+                "name",
+                "cashPrice",
+                "priceDose",
+                "weight",
+                "portion",
+                "capsules",
+                {
+                  flavorsCount: (q) => q.productsFlavors.count(),
+                }
+              )
+              .limit(5),
+        });
+    },
+    {
+      params: t.Object({
+        slug: t.String(),
+      }),
+    }
+  );
